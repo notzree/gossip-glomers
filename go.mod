24a18e162508607a7c08485c78068a1838223ebb
notzree <notzree@gmail.com>
2024-08-09 22:13:38 -0400

    first 2 challenges

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/notzree/gossip-glomers/v2
+
+go 1.22.5
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20240408130303-0186f398f965
+)
---
+++ handlers/uuid-generation.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/google/uuid"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+type Handler struct {
+	Node *maelstrom.Node
+}
+
+func (h *Handler) GenerateUuid(msg maelstrom.Message) error {
+	var body map[string]any
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+	body["type"] = "generate_ok"
+
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+
+	body["id"] = uuid.String()
+	return h.Node.Reply(msg, body)
+}
